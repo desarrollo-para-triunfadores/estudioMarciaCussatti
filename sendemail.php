e5a51dcd89862f0fa693568b2b38a89901e6bb90
Hacho <hacho.kuszniruk@gmail.com>
2016-09-26 00:05:37 -0300

    corregido el envio de mails

--- sendemail.php
+++ sendemail.php
@@ -1,18 +1,29 @@
 <?php
-$name       = @trim(stripslashes($_POST['nombre'])); 
-$from       = @trim(stripslashes($_POST['email'])); 
-$subject    = @trim(stripslashes($_POST['subject'])); 
-$message    = @trim(stripslashes($_POST['message'])); 
-$to   		= 'm.cussatti@gmail.com';//replace with your email
 
-$headers   = array();
-$headers[] = "MIME-Version: 1.0";
-$headers[] = "Content-type: text/plain; charset=iso-8859-1";
-$headers[] = "From: {$name} <{$from}>";
-$headers[] = "Reply-To: <{$from}>";
-$headers[] = "Subject: {$subject}";
-$headers[] = "X-Mailer: PHP/".phpversion();
+require_once('PHPMailer-master/class.phpmailer.php');
+require_once('PHPMailer-master/class.smtp.php');
 
-mail($to, $subject, $message, $headers);
+$name       = $_POST['name'];
+$from       = $_POST['email'];
+$subject    = $_POST['subject'];
+$message    = $_POST['message'];
 
-die;
+$correo = new PHPMailer();
+$correo->IsSMTP();
+$correo->SMTPAuth = true;
+$correo->SMTPSecure = 'tls';
+$correo->Host = "smtp.gmail.com";
+$correo->Port = 587;
+$correo->Username   = "CORREO DE MARCIA O LO QUE SEA";
+$correo->Password   = "CONTRASEÑA DE ESO DE ARRIBA";
+$correo->SetFrom("m.cussatti@gmail.com", "Robot);
+$correo->AddAddress($from , $name);
+$correo->Subject = $subject;
+$correo->MsgHTML($message);
+
+if(!$correo->Send()) {
+  //echo  "Hubo un error: " . $correo->ErrorInfo;
+   echo '<p class="text-warning">Hubo un problema al eviar su mensaje. Por favor utilice otra de manera de contactarse con nosotros. Disculpe las molestias.</p>';
+} else {
+   echo '<p class="text-success">Gracias por ponerse en contacto con nosotros. Tan pronto como sea posible nos pondremos en contacto con usted.</p>';
+}
